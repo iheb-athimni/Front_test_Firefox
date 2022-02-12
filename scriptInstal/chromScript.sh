8c0409a5b83eb890ab9fd4345353d3f0b3692112
iheb <athimniiheb@gmail.com>
2022-02-12 02:24:31 +0100

    fire fox test

--- scriptInstal/chromScript.sh
+++ scriptInstal/chromScript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://download.mozilla.org/?product=firefox-devedition-latest&os=linux64&lang=en-US
 sudo apt install ./google-chrome-stable_current_amd64.deb
